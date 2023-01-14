6a300b16977f3210c593b4c5a2198b678ec3d021
KaranGowda21 <121274810+KaranGowda21@users.noreply.github.com>
2023-01-14 14:25:41 +0530

    This is new Project

---
+++ WiproTensNG/src/main/java/WiproTensNG/WiproTensNG/App.java
@@ -0,0 +1,13 @@
+package WiproTensNG.WiproTensNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
